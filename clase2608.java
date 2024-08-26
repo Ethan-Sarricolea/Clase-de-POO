a9c7e5ec2fe18a3321abea7849d102a97e5df688
Ethan Yahel Sarrivolea Cortés <esarricolea@gmail.com>
2024-08-26 13:44:55 -0600

    Clase de 26/08/2024

--- clase2608.java
+++ clase2608.java
@@ -2,8 +2,33 @@
  * Codigo de la clase de Programacion orientada a objetos LIS 301 - UV
  */
 
+// ejemplo de importacion import java.util.Scanner;
+
+import java.util.Scanner;
+
 public class clase2608{
     public static void main(String[] args){
-        
+        System.out.println("What is your name?");
+
+        // recepcion de datos
+        // instancia de la clase scanner en la var input
+        Scanner input = new Scanner(System.in);
+        String respuesta;
+        respuesta = input.nextLine();
+        try {
+            input.close();
+        } catch(Exception e){
+            System.out.println("Error");
+        }
+        System.out.printf("Hi %s\nsee this...\n",respuesta);
+
+        // cast (conversion de tipo)
+        int number = (int) (System.currentTimeMillis() % 10);
+        System.out.printf("%d + %d = %d",number,5,number+5);
+
+        // Los datos primitivos se almacenan en stack (PILA), el contenido de un dato estructurado en heap
+        // ejemplo input tiene su nombre en el stack y en heap sus datos
     }
 }
+
+// Polimorfismo = mismo metodo distinto comportamiento
---
+++ parOImpar.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+/*
+ * Crear un codigo que diga si un numero es par o impar
+*/
+
+public class parOImpar {
+    public static void main(String[] args){
+        System.out.println("Coloca un numero");
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        try {
+            input.close();
+        } catch (Exception e) {
+
+        }
+        System.out.printf("El numero %d es %s",
+            number,(number%2==0)? "Par" : "Impar");
+    }
+}
